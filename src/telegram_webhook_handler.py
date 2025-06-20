2ff2ebed662feb9f7ea7e74c884775a45f3ff03c
Matheus Santos <matheusgabriel.18@gmail.com>
2025-06-19 22:27:21 -0300

    refactor: add loguru type import

--- src/telegram_webhook_handler.py
+++ src/telegram_webhook_handler.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
 
+from loguru import Logger
+
 from core.logger import L
 from core.settings import settings
 from services.telegram_service import deal_with_webhook_message
@@ -42,7 +44,7 @@ def validate_secret_token(secret_token: str) -> bool:
     return secret_token == settings.TELEGRAM_WEBHOOK_SECRET_TOKEN
 
 
-async def main_logic(payload: dict, *, logger) -> None:
+async def main_logic(payload: dict, *, logger: Logger) -> None:
     message = payload.get("message")
     if not message:
         logger.info("No message found in the payload. Ignoring.")
