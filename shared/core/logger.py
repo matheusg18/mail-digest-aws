c85e0191dbec1a9d60f417d9a4c897c98401ef5e
Matheus Santos <matheusgabriel.18@gmail.com>
2025-07-16 16:48:03 -0300

    refactor: remove request_id from logger format

--- shared/core/logger.py
+++ shared/core/logger.py
@@ -14,7 +14,6 @@ if settings.LOG_FORMAT == "text":
         format=(
             "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
             "<level>{level: <8}</level> | "
-            "<cyan>{extra[request_id]}</cyan> | "
             "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> -"
             " <level>{message}</level>"
         ),
